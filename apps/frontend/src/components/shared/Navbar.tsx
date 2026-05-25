b839c7509b367c918376cbedff3abd6bda72b8c3
Dinh Tan <DTan084@users.noreply.github.com>
2026-05-25 12:07:34 +0700

    chore(frontend): remove admin payments console and related navigation

--- apps/frontend/src/components/shared/Navbar.tsx
+++ apps/frontend/src/components/shared/Navbar.tsx
@@ -123,11 +123,6 @@ export function Navbar() {
                   </Link>
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild>
-                  <Link href="/admin/payments" className="cursor-pointer">
-                    Payments
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem asChild>
                   <Link href="/admin/revenue" className="cursor-pointer">
                     Revenue
                   </Link>
