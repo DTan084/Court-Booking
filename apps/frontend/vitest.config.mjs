5d671bca8989b0428c2f8139258332304421b768
Dinh Tan <DTan084@users.noreply.github.com>
2026-05-29 16:19:59 +0700

    chore(frontend): consolidate vitest config

--- apps/frontend/vitest.config.mjs
+++ apps/frontend/vitest.config.mjs
@@ -1,7 +1,11 @@
-const path = require('path');
-const { defineConfig } = require('vitest/config');
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vitest/config';
 
-module.exports = defineConfig({
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
   root: __dirname,
   test: {
     environment: 'jsdom',
